86e344f4f145efc4678755d56634f38b79fd8ebc
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-09-28 20:10:37 +0530

    configured product endpoint in program.cs

--- Snap-N-Shop-API/Program.cs
+++ Snap-N-Shop-API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<MyDbContext>(
 var app = builder.Build();
 
 app.MapCustomerEndpoints();
+app.MapProductEndpoints();
 app.MapGet("/", () => "Hello from Snap-N-Shop API - backend is up hello");
 
 app.MapGet("/health", () => Results.Ok(new { status = "ok", timestamp = DateTime.UtcNow }));
