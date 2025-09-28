91f6ce80c040834cd5668df3eac7da93fe83bfeb
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-09-28 22:57:11 +0530

    configured order endpoint

--- Snap-N-Shop-API/Program.cs
+++ Snap-N-Shop-API/Program.cs
@@ -18,6 +18,7 @@ var app = builder.Build();
 app.MapCustomerEndpoints();
 app.MapProductEndpoints();
 app.MapCartEndpoints();
+app.MapOrderEndpoints();
 app.MapGet("/", () => "Hello from Snap-N-Shop API - backend is up hello");
 
 app.MapGet("/health", () => Results.Ok(new { status = "ok", timestamp = DateTime.UtcNow }));
