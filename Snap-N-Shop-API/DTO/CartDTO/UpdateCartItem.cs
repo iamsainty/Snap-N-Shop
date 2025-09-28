622682e5fd1472134045151f458578f0646e5bc4
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-09-28 22:00:18 +0530

    updated cart DTO and added update cart DTO

--- Snap-N-Shop-API/DTO/CartDTO/UpdateCartItem.cs
+++ Snap-N-Shop-API/DTO/CartDTO/UpdateCartItem.cs
@@ -1,10 +1,11 @@
 namespace Snap_N_Shop_API.DTO.CartDTO
 {
-    public class AddToCartRequest
+    public class UpdateCartItemRequest
     {
         public int ProductId { get; set; }
+        public bool IncQty { get; set; } = false;
     }
-    public class AddToCartResponse
+    public class UpdateCartItemResponse
     {
         public bool Success { get; set; } = false;
         public string Message { get; set; } = string.Empty;
