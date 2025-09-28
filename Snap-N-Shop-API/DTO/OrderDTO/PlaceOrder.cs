c7533a8bab034b14aa7dfdb20eb57ef671cac3ca
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-09-28 22:56:04 +0530

    added place order DTO

---
+++ Snap-N-Shop-API/DTO/OrderDTO/PlaceOrder.cs
@@ -0,0 +1,8 @@
+namespace Snap_N_Shop_API.DTO.OrderDTO
+{
+    public class PlaceOrderResponse
+    {
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+    }
+}
