0d9fd2c7ee525ed89c41bd37332f2cb01a05ec9d
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-09-29 19:51:55 +0530

    added home component

--- Snap-N-Shop-Client/src/app/app.routes.ts
+++ Snap-N-Shop-Client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : '', component : HomeComponent}
+];
+
+@NgModule({
+    imports : [RouterModule.forRoot(routes)],
+    exports : [RouterModule]
+})
+export class AppRoutesModule {}
