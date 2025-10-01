37f6f49628eee863aa24b87608efcda1d219005c
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-10-01 19:56:11 +0530

    fetching all products and listing on browse

--- Snap-N-Shop-Client/src/app/pages/browse/browse.component.ts
+++ Snap-N-Shop-Client/src/app/pages/browse/browse.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-browse',
-  imports: [ MatIconModule ],
+  imports: [ MatIconModule, CommonModule ],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css'
 })
 export class BrowseComponent {
+  products: any[] = [];
+  ngOnInit() {
+    this.getProducts().then(() => {
+      console.log(this.products);
+    });
+  }
+  async getProducts() {
+    const serverUrl = 'http://0.0.0.0:80';
+    const url = `${serverUrl}/product/all-product`;
 
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data: any = await response.json();
+    this.products = data.products;
+  }
 }
