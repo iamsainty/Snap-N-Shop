98319c26382476a54b108f9b2765a212d63bfd1b
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-10-01 20:46:20 +0530

    fetching category wise products

--- Snap-N-Shop-Client/src/app/pages/browse/category/category.component.ts
+++ Snap-N-Shop-Client/src/app/pages/browse/category/category.component.ts
@@ -1,21 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
 export class CategoryComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   category: string = '';
 
+  allowedCategories: string[] = ['groceries', 'smartphones', 'sports', 'kitchen', 'clothing', 'footwear'];
+
   ngOnInit(): void {
-    this.category = this.route.snapshot.params['category'];
+    this.category = this.route.snapshot.params['category'].toLowerCase();
+    if(!this.allowedCategories.includes(this.category)) {
+      this.router.navigate(['/browse']);
+    }
     console.log(this.category);
+    this.getProducts();
   }
 
+  products: any[] = [];
+  async getProducts() {
+    const serverUrl = 'http://0.0.0.0:80';
+    const url = `${serverUrl}/product/category-product`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ categoryName: this.category })
+    });
+    const data: any = await response.json();
+    console.log(data);
+    this.products = data.products;
+  }
 }
