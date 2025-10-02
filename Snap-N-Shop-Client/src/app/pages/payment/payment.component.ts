48c6a50378c87afbd4b214852ebd3d410186e0d8
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-10-02 14:12:18 +0530

    added UI to payment page

--- Snap-N-Shop-Client/src/app/pages/payment/payment.component.ts
+++ Snap-N-Shop-Client/src/app/pages/payment/payment.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-payment',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
 
+  serverUrl : string = 'http://0.0.0.0:80';
+  router: Router = inject(Router);
+
+  ngOnInit(): void {
+    this.loadCustomer();
+  }
+
+  public loadCustomer() {
+    const customerToken = localStorage.getItem('customerToken');
+    if(!customerToken){
+      this.router.navigate(['/auth']);
+    }
+  }
+
+
+  public goToCart() {
+    this.router.navigate(['/cart']);
+  }
+
+  public placeOrder() {
+    this.router.navigate(['/orders']);
+  }
 }
