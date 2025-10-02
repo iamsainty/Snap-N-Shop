285b818c9257efdc3b1f8c57a0d3ead05d38eaac
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-10-02 23:16:08 +0530

    fixes and deployement trials

--- Snap-N-Shop-Client/src/app/pages/home/home.component.ts
+++ Snap-N-Shop-Client/src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  router: Router = inject(Router);
+  products: any[] = [];
+  serverUrl: string = 'http://0.0.0.0:80';
 
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  async loadProducts() {
+    const url = `${this.serverUrl}/product/all-product`;
+    const response = await fetch(url, { method: 'GET' });
+    const data: any = await response.json();
+    this.products = data.products;
+  }
+
+  goToSignIn() {
+    this.router.navigate(['/auth']);
+  }
 }
